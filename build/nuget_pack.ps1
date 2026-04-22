44e9d66b4666cc12ba83f4a721deef5e9d0f97ea
ema <mccoy39082@163.com>
2026-04-23 03:22:27 +0800

    Add NuGet pack script and .gitignore

---
+++ build/nuget_pack.ps1
@@ -0,0 +1,26 @@
+﻿Set-Location $PSScriptRoot
+
+Write-Host @"
+███╗   ██╗██╗   ██╗ ██████╗ ███████╗████████╗
+████╗  ██║██║   ██║██╔════╝ ██╔════╝╚══██╔══╝
+██╔██╗ ██║██║   ██║██║  ███╗█████╗     ██║   
+██║╚██╗██║██║   ██║██║   ██║██╔══╝     ██║   
+██║ ╚████║╚██████╔╝╚██████╔╝███████╗   ██║   
+╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   
+"@
+
+$projects = @(
+    "..\src\UnblockZoneIdentifier"
+)
+
+foreach ($proj in $projects) {
+    Push-Location $proj
+    Write-Host "Processing $proj..."
+    dotnet restore
+    dotnet build -c Release
+    dotnet pack -c Release -o ../../build/
+    Pop-Location
+}
+
+Write-Host "`nPress any key to exit..."
+[void][System.Console]::ReadKey($true)
